45c8a129b85e790b70f7eac2e93bdb096a3d3f6a
christopherhanke <christopherhanke@googlemail.com>
2025-01-05 14:22:30 +0100

    adding test names to test tables

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -3,57 +3,64 @@ package auth
 import (
 	"errors"
 	"net/http/httptest"
+	"reflect"
 	"strings"
 	"testing"
 )
 
 func TestGetAPIKey(t *testing.T) {
-	tests := []struct {
+	tests := map[string]struct {
 		input, expected string
 	}{
-		{"ApiKey 123456789", "123456789"},
-		{"ApiKey ThisIsAnAPIkey", "ThisIsAnAPIkey"},
+		"valid key - numbers": {"ApiKey 123456789", "123456789"},
+		"valid key - chars":   {"ApiKey ThisIsAnAPIkey", "ThisIsAnAPIkey"},
 	}
 
-	for _, test := range tests {
-		req := httptest.NewRequest("GET", "/", strings.NewReader(""))
-		req.Header.Add("Content-Type", "application/json")
-		req.Header.Add("Authorization", test.input)
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			req := httptest.NewRequest("GET", "/", strings.NewReader(""))
+			req.Header.Add("Content-Type", "application/json")
+			req.Header.Add("Authorization", test.input)
 
-		stringToken, err := GetAPIKey(req.Header)
-		if err != nil {
-			t.Errorf("Getting API key failed: %s", err)
-			return
-		}
-		if stringToken != test.expected {
-			t.Errorf("API key didn't match\nExpected: %s\nActual: %s", test.expected, stringToken)
-		}
+			stringToken, err := GetAPIKey(req.Header)
+			if err != nil {
+				t.Fatalf("Getting API key failed: %s", err)
+			}
+
+			if !reflect.DeepEqual(stringToken, test.expected) {
+				t.Fatalf("API key didn't match\nExpected: %s\nActual:   %s", test.expected, stringToken)
+			}
+		})
+
 	}
 
 }
 
 func TestFalseAPIKey(t *testing.T) {
-	tests := []struct {
-		input string
-		fails error
+	tests := map[string]struct {
+		input    string
+		expected error
 	}{
-		{"no ApiKey", errors.New("malformed authorization header")},
-		{"ApiKey", errors.New("malformed authorization header")},
-		{"", errors.New("no authorization header included")},
+		"Keyword trailing": {"no ApiKey", errors.New("malformed authorization header")},
+		"Key missing":      {"ApiKey", errors.New("malformed authorization header")},
+		"Keyword missing":  {"", errors.New("no authorization header included")},
 	}
 
-	for _, test := range tests {
-		req := httptest.NewRequest("GET", "/", strings.NewReader(""))
-		req.Header.Add("Content-Type", "application/json")
-		req.Header.Add("Authorization", test.input)
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			req := httptest.NewRequest("GET", "/", strings.NewReader(""))
+			req.Header.Add("Content-Type", "application/json")
+			req.Header.Add("Authorization", test.input)
 
-		stringToken, err := GetAPIKey(req.Header)
-		if err == nil {
-			t.Errorf("Getting API key should have failed. Got token: %s", stringToken)
-			return
-		}
-		if errors.Is(err, test.fails) {
-			t.Errorf("Error didn't match\nExpected: %s\nActual: %s", test.fails, err)
-		}
+			stringToken, err := GetAPIKey(req.Header)
+			if err == nil {
+				t.Errorf("Getting API key should have failed. Got token: %s", stringToken)
+				return
+			}
+			if errors.Is(err, test.expected) {
+				t.Errorf("Error didn't match\nExpected: %#v\nActual:   %#v", test.expected, err)
+			}
+		})
+
 	}
 }
